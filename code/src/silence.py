6a073a026b0f70430fb774f844ceb5536a1cd96f
dwolffram <25206417+dwolffram@users.noreply.github.com>
2025-09-02 17:07:32 +0200

    refactor training code

---
+++ code/src/silence.py
@@ -0,0 +1,17 @@
+import logging
+import warnings
+
+
+def silence():
+    warnings.filterwarnings(
+        "ignore",
+        message="X does not have valid feature names, but LGBMRegressor was fitted with feature names",
+        category=UserWarning,
+        module="sklearn.utils.validation",
+    )
+
+    warnings.filterwarnings("ignore", message=".*does not have many workers.*")
+    warnings.filterwarnings("ignore", message=".*pin_memory.*")
+    warnings.filterwarnings("ignore", message=r"pkg_resources is deprecated", category=UserWarning)
+
+    logging.getLogger("pytorch_lightning.utilities.rank_zero").setLevel(logging.WARNING)
