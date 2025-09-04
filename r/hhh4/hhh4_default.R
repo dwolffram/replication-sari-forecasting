0ef923ecd9cda96022b9d8b80bd056ae47d5c3d8
dwolffram <25206417+dwolffram@users.noreply.github.com>
2025-09-04 17:49:37 +0200

    add hhh4 code and forecasts

---
+++ r/hhh4/hhh4_default.R
@@ -0,0 +1,28 @@
+# Apply the KIT-hhh4 model with default settings (see below)
+# Author: Johannes Bracher, johannes.bracher@kit.edu
+
+######################################################
+# Settings specific to this file:
+label <- "hhh4_default"
+# not excluding the COVID period in this file
+exclusion_period <- as.Date(NULL)
+# forecasts for different nowcast paths are aggregated by linear pool
+aggregation_paths <- "linear pool"
+# nowcast quantiles are ordered rather than shuffled
+shuffle_paths <- FALSE
+# last observation is not omitted
+skip_last = FALSE
+
+######################################################
+# get packages and functions:
+library(surveillance)
+library(hhh4addon)
+source(here("r", "hhh4", "functions_hhh4.R"))
+
+######################################################
+# get global setup shared between all versions of hhh4:
+source(here("r", "hhh4", "setup_hhh4.R"))
+
+######################################################
+# run core code that can generate hhh4 forecasts for most settings
+source(here("r", "hhh4", "core_hhh4.R"))
