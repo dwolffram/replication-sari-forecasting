c2ffc284cc0bb49e220a0e337db30bc03cedc6bd
dwolffram <25206417+dwolffram@users.noreply.github.com>
2025-09-08 12:14:32 +0200

    clean up

---
+++ r/compute_ensemble.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+
+models <- c("KIT-hhh4", "lightgbm", "tsmixer")
+
+load_member_models <- function(models, forecast_date) {
+  df <- data.frame()
+  for (model in models) {
+    filepath <- here(
+      "forecasts",
+      paste0(
+        model,
+        "/",
+        forecast_date,
+        "-icosari-sari-",
+        model,
+        ".csv"
+      )
+    )
+    df_temp <- read_csv(filepath) %>%
+      mutate(model = model) %>%
+      filter(type == "quantile")
+
+    df <- rbind(df, df_temp)
+  }
+  return(df)
+}
+
+compute_ensemble <- function(models, forecast_date) {
+  df <- load_member_models(models, forecast_date)
+
+  df_ensemble <- df %>%
+    group_by(
+      location,
+      age_group,
+      forecast_date,
+      target_end_date,
+      horizon,
+      type,
+      quantile
+    ) %>%
+    summarize(value = mean(value), .groups = "drop") %>%
+    mutate(
+      age_group = factor(
+        age_group,
+        levels = c("00+", "00-04", "05-14", "15-34", "35-59", "60-79", "80+")
+      )
+    ) %>%
+    arrange(age_group)
+
+  return(df_ensemble)
+}
+
+
+# Example
+forecast_date <- "2023-12-14"
+df <- load_member_models(models, forecast_date)
+df <- compute_ensemble(models, forecast_date)
+
+
+# Compute all
+forecast_dates <- as.character(seq(
+  from = as.Date("2023-12-14"),
+  to = as.Date("2024-09-19"),
+  by = 7
+))
+
+
+for (forecast_date in forecast_dates) {
+  tryCatch(
+    {
+      df_ensemble <- compute_ensemble(models, forecast_date)
+
+      ensemble_path <- here(
+        "forecasts",
+        "ensemble",
+        paste0(
+          forecast_date,
+          "-icosari-sari-ensemble.csv"
+        )
+      )
+
+      write_csv(df_ensemble, ensemble_path)
+      message("✓ Finished ", forecast_date)
+    },
+    error = function(e) {
+      message("✗ Failed on ", forecast_date, ": ", conditionMessage(e))
+    }
+  )
+}
